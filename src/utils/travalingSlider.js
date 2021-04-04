43b1d5b08803e4e1b2885e4ba5403d8b7e0ca105
TankistPro <nakol.nayk@mail.ru>
2021-04-04 15:30:00 +0300

    fix slider

--- src/utils/travalingSlider.js
+++ src/utils/travalingSlider.js
@@ -1,35 +1,57 @@
 module.exports = function slider() {
-  window.onload = () => {
-    const arrowLeft = document.querySelector(".arrow-left"),
+  const arrowLeft = document.querySelector(".arrow-left"),
       arrowRight = document.querySelector(".arrow-right");
-    const sliderLenta = document.querySelector(".slider-lenta");
-    const sliderWrapper = document.querySelector(
-      ".traveling-wrapper__slider-tape"
-    ).offsetWidth;
-    let count = 0;
+  const sliderLenta = document.querySelector(".slider-lenta");
+  const sliderWrapper = document.querySelector(
+  ".traveling-wrapper__slider-tape"
+  ).offsetWidth;
+  const sliderLength = document.querySelectorAll(".traveling-wrapper__slider-tape__item").length
+  let count = 0;
 
-    // console.log("Ширина области просмотра = " + sliderWrapper);
-    // console.log("Ширина ленты = " + sliderLenta.clientWidth);
+  console.log("Ready")
 
-    const changeSlide = (event) => {
-      const arrow = event.target.dataset.side;
-      sliderLenta.style.transition = ".5s all";
-      if (arrow === "right") {
-        if (sliderWrapper * (count + 1) < sliderLenta.clientWidth) {
-          count++;
-          sliderLenta.style.transform = `translateX(${
-            -sliderWrapper * count
-          }px)`;
-        }
-      } else if (arrow === "left") {
-        if (sliderWrapper * count > 0) {
-          count--;
-          sliderLenta.style.transform = `translateX(${-sliderWrapper * count}px)`;
-        }
+
+  const hideArrow = () => {
+    if (count + 1 == sliderLength) {
+      arrowRight.style.opacity = "0";
+      arrowRight.style.pointerEvents = "none";
+    } else {
+      arrowRight.style.opacity = "1";
+      arrowRight.style.pointerEvents = "initial";
+    }
+    
+    if (count === 0) {
+      arrowLeft.style.opacity = "0";
+      arrowLeft.style.pointerEvents = "none";
+    } else {
+      arrowLeft.style.opacity = "1";
+      arrowLeft.style.pointerEvents = "initial";
+    }
+    
+  }
+  // console.log("Ширина области просмотра = " + sliderWrapper);
+  // console.log("Ширина ленты = " + sliderLenta.clientWidth);
+  hideArrow()
+  const changeSlide = (event) => {
+    const arrow = event.target.dataset.side;
+    sliderLenta.style.transition = ".5s all";
+    if (arrow === "right") {
+      if (sliderWrapper * (count + 1) < sliderLenta.clientWidth) {
+        count++;
+        sliderLenta.style.transform = `translateX(${
+          -sliderWrapper * count
+        }px)`;
+        hideArrow();
       }
-    };
-
-    arrowLeft.addEventListener("click", changeSlide);
-    arrowRight.addEventListener("click", changeSlide);
+    } else if (arrow === "left") {
+      if (sliderWrapper * count > 0) {
+        count--;
+        sliderLenta.style.transform = `translateX(${-sliderWrapper * count}px)`;
+        hideArrow();
+      }
+    }
   };
+
+  arrowLeft.addEventListener("click", changeSlide);
+  arrowRight.addEventListener("click", changeSlide);
 };
