91eb936e6cc31e19e57a894061c2d1ae04184d55
AnnaStimp <58040522+AnnaStimp@users.noreply.github.com>
2021-04-05 15:38:09 +0300

    upgrade inventory

--- src/views/Inventory/Inventory.vue
+++ src/views/Inventory/Inventory.vue
@@ -22,8 +22,19 @@
           <div class="inventory__listOfMatters__header">
               <div class="inventory__listOfMatters__header__name">Инвентарь</div>
               <div class="inventory__listOfMatters__header__search">
-                  <input type="text">
+                  <input type="text" placeholder="Поиск предмета...">
                   <img src="../../assets/images/inventory/search.svg" alt="">
+              </div>
+          </div>
+          <div class="inventory__listOfMatters__main">
+              <div class="inventory__listOfMatters__main__matter">
+                  <div class="inventory__listOfMatters__main__matter__name">
+                      <p>Потертая шлапа</p>
+                      <img src="../../assets/images/inventory/cork-hat.png" alt="">
+                  </div>
+                  <div class="inventory__listOfMatters__main__matter__power">
+                      <p>Броня: <span>10</span></p>
+                  </div>
               </div>
           </div>
       </div>
